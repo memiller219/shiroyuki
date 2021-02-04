0ba984a27abe3b493e92ea1929195b3441e53b20
Michael <memiller219@gmail.com>
2021-02-04 12:17:44 -0800

    added collection nested route in shop page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,22 @@
 import React from "react";
-import { connect } from 'react-redux';
-import { createStructuredSelector } from "reselect";
-import { selectShopCollections } from "../../redux/shop/shop.selectors";
+import { Route } from 'react-router-dom'
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import Collection from '../collection/collection.component';
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
-const ShopPage = ({ collections }) => {
+
+const ShopPage = ({ match }) => {
    
     return (
       <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route exact path={`${match.path}/:collectionId`} component={Collection} />
       </div>
     );
   }
 
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections
-})
 
 
 
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,23 @@
-import {createSelector} from 'reselect';
+import { createSelector } from "reselect";
 
-const selectShop = state => state.shop;
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  skis: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5,
+};
+
+const selectShop = (state) => state.shop;
 
 export const selectShopCollections = createSelector(
-    [selectShop],
-    shopData => shopData.collections
-)
+  [selectShop],
+  (shopData) => shopData.collections
+);
+
+export const selectCollection = (collectionUrlParam) =>
+  createSelector([selectShopCollections], (collections) =>
+    collections.find(
+      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+    )
+  );
